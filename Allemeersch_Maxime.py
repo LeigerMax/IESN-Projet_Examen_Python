8a13d45a46f2794a10143ff99604d1ab411f4277
LeigerMax <puissantmax12@gmail.com>
2020-05-04 22:47:34 +0200

    exceptions values

--- Allemeersch_Maxime.py
+++ Allemeersch_Maxime.py
@@ -48,16 +48,8 @@ class Enemy:
                     's': (1, 0),
                     'd': (0, 1)}
 
-    def __init__(self, name,start=(5, 5)):
-        self._name = name
+    def __init__(self,start=(5, 5)):
         self._position = start
-
-    @property
-    def name(self):
-        return self._name
-    @name.setter
-    def name(self, new_name):
-        self._name = new_name
 
     @property
     def position(self):
@@ -172,7 +164,7 @@ class Game:
     def candyPartyOrNOT(self, new_candyPartyOrNOT):
         self._candyPartyOrNOT = new_candyPartyOrNOT
 
-    # Dessine le plateau
+    #Dessine le plateau
     def draw(self):
         for wallLineTop in range(self._board_size):
             print("◼", end=" ")
@@ -211,23 +203,23 @@ class Game:
             #print("\033[36;1m◼\033[0m", end=" ") #wallLineLow
         print()
 
-    # Fait apparaitre un bonbon
+    #Fait apparaitre un bonbon
     def pop_candy(self):
         new_candy = (random.choice(range(self._board_size)), random.choice(range(self._board_size)))
-        if new_candy not in self._candies:
+        if new_candy not in self._candies and new_candy not in self.player1._position  and new_candy not in self.enemy1._position and new_candy not in self.enemy2._position:
             self._candies.append(new_candy)
 
-    # CandyParty
+    #CandyParty
     def pop_candyParty(self):
         candytSpawn = random.randint(1, 5)
         print("Vous avez trouver un bonbon magique ! ", candytSpawn, " bonbons ont apparut")
         while (candytSpawn != 0):
             new_candy = (random.choice(range(self._board_size)), random.choice(range(self._board_size)))
-            if new_candy not in self._candies:
+            if new_candy not in self._candies and new_candy not in self.player1._position and new_candy not in self.enemy1._position and new_candy not in self.enemy2._position:
                 self._candies.append(new_candy)
             candytSpawn = candytSpawn -1
 
-    # Permet au joueur de se téléporter à l'opposé
+    #Permet au joueur de se téléporter à l'opposé
     def check_Position_TP(self):
         ListA = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         ListB = [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
@@ -311,7 +303,7 @@ class Game:
                 elif self.enemy2._position == (self._board_size, CheckFirst):  # Vers le bas Enemy
                     self.enemy2._position = (self._board_size-1, CheckFirst)
 
-    # Regarde s'il y a un bonbon à prendre (et le prend)
+    #Regarde s'il y a un bonbon à prendre (et le prend)
     def check_candy(self):
         pointsWin = random.randint(1, 3)
         CandyPartyTrue = random.randint(1, 5)
@@ -335,8 +327,7 @@ class Game:
             self._candies.remove(self.enemy2._position)
 
 
-        # Regarde s'il y a un enemy
-
+    #Regarde s'il y a un enemy
     def check_Enemy(self):
         if self.player1._position == self.enemy1._position or self.player1._position == self.enemy2._position :
             if self.player1._points <= 10:
@@ -354,7 +345,7 @@ class Game:
                 self.player2._points -= self.player2._points
 
     #Enregistre le score
-    def StoreLeaderBoard(self):
+    def storeLeaderBoard(self):
         fi = open('Leaderboard.txt','a')
         fi.write(str(self.player1._points)) #Enregistre le score Player1
         fi.write(' ')
@@ -366,6 +357,7 @@ class Game:
         fi.write('\n')
         fi.close
 
+    #Affiche le score
     def displayLeaderboard(self):
         print("--- Score ---")
         fi = open('Leaderboard.txt','r')
@@ -375,14 +367,16 @@ class Game:
             print(line.strip())
         self.menu()
 
-
+    #Menu
     def menu(self):
         print("--- Menu ---")
         print("Niveau Normal (1): 1min, 2 ennemi, mur bloquer et bonus")
-        print("Personnaliser (2) ")
+        print("Personnalisé (2) ")
         print("Tableau des scores  (3) ")
         try:
             self._choixLevel = int(input("Niveau : "))
+            while self._choixLevel != 1 and self._choixLevel != 2 and self._choixLevel != 3:
+                self._choixLevel = int(input("Numéro invalide! Niveau : "))
             if self._choixLevel == 1 :
                 self.play()
             elif self._choixLevel == 2:
@@ -466,7 +460,7 @@ class Game:
         print("----- Terminé -----")
         print(self.player1.name, "vous avez", self.player1._points, "points")
         print(self.player2.name,"vous avez", self.player2._points, "points")
-        self.StoreLeaderBoard()
+        self.storeLeaderBoard()
         #self.menu()
 
     @staticmethod
@@ -481,8 +475,8 @@ if __name__ == "__main__":
     NamePlayer2 = input("Nom du joueur 2 : ")
     p1 = Player(NamePlayer1)
     p2 = Player(NamePlayer2,(0, 9))
-    e1 = Enemy("Enemy1",(7,2))
-    e2 = Enemy("Enemy2",(7,7))
+    e1 = Enemy((7,2))
+    e2 = Enemy((7,7))
     g = Game(p1,p2,e1,e2)
     g.menu()
 
